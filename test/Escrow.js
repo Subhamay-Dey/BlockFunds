d92c788bb40381a6ed84bf392f99d34a733525fb
Subhamay-Dey <ncdey1966@gmail.com>
2024-09-28 03:09:36 +0530

    contract Escrow is successfully coded and it is returning the address ✅, used ether js.

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,11 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    it('saves the address', async() => {
+        const RealEstate = await ethers.getContractFactory("RealEstate")
+        const realEstate = await RealEstate.deploy()
 
+        console.log(realEstate.address);
+        
+    })
 })
